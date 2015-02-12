91971ec8acdaf482d11543af4c4951f1eaa39d6c
Matthew Moore <matthewemmoore@googlemail.com>
2015-02-12 11:56:56 +0000

    Added Jared's Code to unity project

---
+++ Blueberry/Assets/Scripts/Pause.js
@@ -0,0 +1,118 @@
+﻿var mainMenuSceneName : String;
+var pauseMenuFont : Font;
+private var pauseEnabled = false;		
+//var someTexture : Texture2D;
+//guiTexture.texture = someTexture;
+		
+
+function Start(){
+	pauseEnabled = false;
+	Time.timeScale = 1;
+	AudioListener.volume = 1;
+	Screen.showCursor = false;
+}
+
+function Update(){
+
+	//check if pause button (escape key) is pressed
+	if(Input.GetKeyDown("escape"))
+	{
+	
+		//check if game is already paused		
+		if(pauseEnabled == true)
+		{
+			//unpause the game
+			pauseEnabled = false;
+			Time.timeScale = 1;				// Resumes the screen flow and unfreezes 
+			AudioListener.volume = 1;
+			Screen.showCursor = false;			
+		}
+		
+		//else if game isn't paused, then pause it
+		else if(pauseEnabled == false)
+		{
+			pauseEnabled = true;			
+			AudioListener.volume = 0;		// Kills the sound.
+			Time.timeScale = 0;				//freezes screen.
+			Screen.showCursor = true; 		// shows the cursor.
+		}
+	}
+}
+
+private var showGraphicsDropDown = false;
+
+function OnGUI(){
+
+GUI.skin.box.font = pauseMenuFont;
+GUI.skin.button.font = pauseMenuFont;
+GUI.skin.label.font = pauseMenuFont;
+
+
+	if(pauseEnabled == true)
+	{
+		
+	
+		
+		//Make a background box
+		GUI.Label(Rect(Screen.width /2 - 17,Screen.height /2 - 100,250,200), "Pause Menu");
+		
+		//Make Main Menu button
+		if(GUI.Button(Rect(Screen.width /2 - 100,Screen.height /2 - 50,250,50), "Main Menu"))
+		{
+			Application.LoadLevel(mainMenuSceneName);// This will need changing to reflect the correct level.
+		}
+		
+			//Make Change Graphics Quality button
+			if(GUI.Button(Rect(Screen.width /2 - 100,Screen.height /2 ,250,50), "Change Graphics Quality"))
+			{
+			
+			if(showGraphicsDropDown == false)
+			{
+				showGraphicsDropDown = true;
+			}
+			else
+			{
+				showGraphicsDropDown = false;
+			}
+		}
+		
+		
+		if(showGraphicsDropDown == true){
+			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 ,250,50), "Fastest"))
+			{
+				QualitySettings.currentLevel = QualityLevel.Fastest;
+			}
+			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 50,250,50), "Fast"))
+			{
+				QualitySettings.currentLevel = QualityLevel.Fast;
+			}
+			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 100,250,50), "Simple"))
+			{
+				QualitySettings.currentLevel = QualityLevel.Simple;
+			}
+			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 150,250,50), "Good"))
+			{
+				QualitySettings.currentLevel = QualityLevel.Good;
+			}
+			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 200,250,50), "Beautiful"))
+			{
+				QualitySettings.currentLevel = QualityLevel.Beautiful;
+			}
+			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 250,250,50), "Fantastic"))
+			{
+				QualitySettings.currentLevel = QualityLevel.Fantastic;
+			}
+			
+			if(Input.GetKeyDown("escape"))
+			{
+				showGraphicsDropDown = false;
+			}
+		}
+		
+		//Make quit game button
+		if (GUI.Button (Rect (Screen.width /2 - 100,Screen.height /2 + 50,250,50), "Quit Game"))
+		{
+			Application.Quit();
+		}
+	}
+}
---
+++ Blueberry/Assets/Scripts/UI AI Aesthetic Picker.js
@@ -0,0 +1,130 @@
+﻿var Ai_Name : String = "";
+var Ai_Number : String = "1";
+var pauseMenuFont : Font;
+private var pauseEnabled = false;	
+var aTexture : Texture;	
+var aTexture1 : Texture;	
+var aTexture2 : Texture;	
+var AI_Picker = 0;
+
+		
+function Start()
+{
+Screen.showCursor = true;	
+}
+
+function Update()
+{
+
+	
+}
+
+function AI_1()
+{
+GUI.DrawTexture(Rect(Screen.width /2.236 - 85 ,Screen.height /8,200,200), aTexture, ScaleMode.ScaleToFit, true, 1.50f);
+}
+
+function AI_2()
+{
+GUI.DrawTexture(Rect(Screen.width /2.236 - 85 ,Screen.height /8,200,200), aTexture1, ScaleMode.ScaleToFit, true, 1.50f);
+}
+
+function AI_3()
+{
+GUI.DrawTexture(Rect(Screen.width /2.236 - 85 ,Screen.height /8,200,200), aTexture2, ScaleMode.ScaleToFit, true, 1.50f);
+}
+
+
+function OnGUI()
+{
+
+
+// Sets font for all used GUI items
+GUI.skin.box.font = pauseMenuFont;
+GUI.skin.button.font = pauseMenuFont;
+GUI.skin.label.font = pauseMenuFont;
+
+		//*****************************************************************************************************************
+		// Different textures will need to be added to the inspector so that the final in game images/textures are correct.
+		//*****************************************************************************************************************
+
+		
+		//If the first button is clicked, text is generated and AI function is ran
+		if(GUI.Button(Rect(Screen.width /3,Screen.height /2 ,150,50), "AI Character 1"))
+		{
+			AI_Picker = 1;
+			Ai_Name = "AI Character: 1!";
+			Screen.showCursor = true;	
+		}
+		
+		//If the second button is clicked, text is generated and AI function is ran
+		if(GUI.Button(Rect((Screen.width /4 *2),Screen.height /2,150,50), "AI Character 3"))
+		{
+			AI_Picker = 2;
+			Ai_Name = "AI Character: 3!";
+			Screen.showCursor = true;	
+		}
+		
+		//If the third button is clicked, text is generated and AI function is ran
+		if(GUI.Button(Rect(Screen.width /2 - 160,Screen.height /2,150,50), "AI Character 2"))
+		{
+			AI_Picker = 3;
+				Ai_Name = "AI Character: 2!";
+			Screen.showCursor = true;	
+			
+		}
+		
+		
+		
+		if(GUI.Button(Rect(Screen.width /2 - 155,Screen.height /1.7,40,40), "1"))
+		{
+			Ai_Number = "1";
+			Screen.showCursor = true;	
+		}
+		if(GUI.Button(Rect(Screen.width /2 - 105,Screen.height /1.7,40,40), "2"))
+		{
+			Ai_Number = "2";
+			Screen.showCursor = true;	
+		}
+		if(GUI.Button(Rect(Screen.width /2 - 55,Screen.height /1.7,40,40), "3"))
+		{
+			Ai_Number = "3";	
+			Screen.showCursor = true;	
+		}
+		
+		
+		// Loads the game.
+		if (GUI.Button (Rect (Screen.width /2.236 - 25 ,Screen.height /2 + 150,80,50), "Play"))
+		{
+			// Load the correct scene/application here.
+		}
+		
+		
+			
+				if ( AI_Picker == 1)
+				{
+				AI_1();
+				}
+				
+				if ( AI_Picker == 2)
+				{
+				AI_2();
+				}
+				
+				if ( AI_Picker == 3)
+				{
+				AI_3();
+				}
+			
+			// The  label for the name above the AI charcter.	
+			GUI.Label(Rect(Screen.width /2 - 140,Screen.height /8,150,50), Ai_Name );
+			//Label for AI number
+			GUI.Label(Rect(Screen.width /2 - 90,Screen.height /2.4,150,50), Ai_Number );
+			
+			GUI.Label(Rect(Screen.width /2 - 130,Screen.height /2.59,150,50), "Number of AI" );
+	
+}
+		
+	
+
+
