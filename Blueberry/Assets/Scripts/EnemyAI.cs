6ef7218cf57fe22b4ef3958d263673e2a4302bcd
Matthew Moore <matthewemmoore@googlemail.com>
2015-02-12 12:09:31 +0000

    Added Connor's work
    Added AI into game
    Blueberry start selection
    Basic scoring
    Basic pickups

---
+++ Blueberry/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	public Transform target;
+	public int moveSpeed;
+	public float rotationSpeed;
+	public int maxDistance;
+	public int alertDistance;
+	public GameObject go;
+
+	//float distance;
+
+	private Transform myTransform;
+	//int alertDistance11;
+	//int alertDistance14;
+
+
+	void Awake() {
+		myTransform = transform;
+		
+	}
+	
+	// Use this for initialization
+	void Start () {
+		 go = GameObject.FindGameObjectWithTag("Player");
+		target = go.transform;
+		//Find the player and get their position. Will need to be made more advanced later as other people may get the virus.
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawLine(target.position, myTransform.position, Color.yellow);
+		//distance = Vector3.Distance(target.position, myTransform.position);
+
+		if (go.GetComponent<VirusScript>().Blueberry == false)
+		{
+		if (Vector3.Distance (target.position, myTransform.position) < alertDistance)
+		{	
+			//If the AI is close enough to notice the player. Will be used for selecting of who to chase after, when that feature is implemented.
+			//This ensure the AI locks onto the player and moves towards him correctly.
+			bool needsWeirdAngThing = Vector3.Angle (Vector3.right, target.position - this.transform.position) > 90;
+			transform.rotation = Quaternion.Lerp (myTransform.rotation, Quaternion.RotateTowards (myTransform.rotation, Quaternion.LookRotation (target.position - transform.position), 90), rotationSpeed);
+			//transform.LookAt(target.position);
+			transform.rotation = Quaternion.Euler (transform.localEulerAngles.x, 90, 270);
+			
+			if (needsWeirdAngThing == true)
+				transform.eulerAngles = new Vector3 (transform.eulerAngles.x, 270, 90);
+			else
+				transform.eulerAngles = new Vector3 (transform.eulerAngles.x, 90, 270);
+		}
+		}
+			if (go.GetComponent<VirusScript>().Blueberry == true)
+			{
+				if (Vector3.Distance (target.position, myTransform.position) < alertDistance)
+				{	
+					//If the AI is close enough to notice the player. Will be used for selecting of who to chase after, when that feature is implemented.
+					//This ensure the AI locks onto the player and moves towards him correctly.
+					bool needsWeirdAngThing = Vector3.Angle (Vector3.right, target.position - this.transform.position) > 90;
+					transform.rotation = Quaternion.Lerp (myTransform.rotation, Quaternion.RotateTowards (myTransform.rotation, Quaternion.LookRotation (target.position - transform.position), 90), rotationSpeed);
+					//transform.LookAt(target.position);
+					transform.rotation = Quaternion.Euler (transform.localEulerAngles.x, 90, 270);
+					
+					if (needsWeirdAngThing == true)
+						transform.eulerAngles = new Vector3 (transform.eulerAngles.x, 270, 90);
+					else
+						transform.eulerAngles = new Vector3 (transform.eulerAngles.x, 90, 270);
+					
+				transform.Rotate(0,180,0);
+				}
+			}
+			//Move towards the player's position.
+			myTransform.position += myTransform.forward*moveSpeed*Time.deltaTime;
+		}
+		
+	}
---
+++ Blueberry/Assets/Scripts/RandomVirus.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomVirus : MonoBehaviour {
+
+	
+	public GameObject[] players;
+	public int InitBlueberry;
+	public GameObject Startvirus;
+	public bool blueberry;
+	float timer = 0f;
+	int points = 0;
+	public List<GameObject> playersList;
+
+
+
+	// Use this for initialization
+	void Start () {
+		//VirusScript virus;
+
+		playersList = new List<GameObject>();
+
+		playersList.AddRange(GameObject.FindGameObjectsWithTag ("Player"));
+		playersList.AddRange(GameObject.FindGameObjectsWithTag ("AI"));
+
+		players = playersList.ToArray();
+
+		//players = players + GameObject.FindGameObjectsWithTag ("Player");
+		InitBlueberry = ChooseAtRandom(players);
+		Startvirus = players[InitBlueberry];
+		Startvirus.GetComponent<VirusScript>().Blueberry = true;
+		//virusscript.Blueberry = true;
+		//players.gameObject.GetComponent<VirusScript>().Blueberry = true;
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		points = 5 * (int)timer;
+		guiText.text = (int)timer + "    seconds                  " + points + "   points";
+	
+	}
+
+
+	public static int ChooseAtRandom (GameObject[] array)
+	{
+		return Random.Range(0, array.Length);
+	}
+
+
+
+}
