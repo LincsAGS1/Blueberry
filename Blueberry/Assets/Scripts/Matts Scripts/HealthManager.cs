21b73587e9df925af4c291bf95947f1ae216decf
Dusty-Meg <matthewemmoore@googlemail.com>
2015-02-18 17:08:47 +0000

    Added Health Manager

---
+++ Blueberry/Assets/Scripts/Matts Scripts/HealthManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthManager : MonoBehaviour 
+{
+
+    public float health = 100;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (this.GetComponent<VirusScript>().Blueberry == true && health > 0)
+        {
+            health -= 0.1f;
+        }
+
+        if (health <=  0)
+        {
+            Debug.Log(this.name + " Died");
+        }
+	}
+}
