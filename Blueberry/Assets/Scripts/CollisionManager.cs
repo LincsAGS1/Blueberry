0a19e8c5a532807ae89d3776cb24284a1302a9d4
Dusty-Meg <matthewemmoore@googlemail.com>
2015-02-18 15:38:47 +0000

    Added Passing of Blueberry
    Blueberry is passed when objects have Blueberry
    Delay to pass back allowing for objects to get away from Blueberry

---
+++ Blueberry/Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionManager : MonoBehaviour 
+{
+
+    public bool collisions = false;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+        
+
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.collider.name.Contains("Enemy") || collision.collider.name.Contains("Player"))
+        {
+            if (this.GetComponent<VirusScript>().Blueberry == true)
+            {
+                if (collisions == false)
+                {
+                    GameObject virusScript = collision.collider.gameObject;
+
+                    virusScript.GetComponent<VirusScript>().Blueberry = true;
+
+                    this.GetComponent<VirusScript>().Blueberry = false;
+
+                    Debug.Log("Passing to " + collision.collider.name.ToString());
+
+                    virusScript.GetComponent<CollisionManager>().collisions = true;
+
+                    StartCoroutine(virusScript.GetComponent<CollisionManager>().wait());
+                }
+            }
+        }
+    }
+
+    public IEnumerator wait() // Runs methods every 10 seconds
+    {
+        yield return new WaitForSeconds(6.0f);
+
+        collisions = false;
+
+        Debug.Log(this.name + " Reset");
+    }
+}
