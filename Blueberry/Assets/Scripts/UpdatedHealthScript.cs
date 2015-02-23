1341a92748c3636e9ee5a4fa56080645d589027d
Jared Pepper <jared737@msn.com>
2015-02-23 14:07:02 +0000

    Created health script
    Made a new health script that destroys object when health hits 0.

--- Blueberry/Assets/Scripts/UpdatedHealthScript.cs
+++ Blueberry/Assets/Scripts/UpdatedHealthScript.cs
@@ -1,39 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HealthManager : MonoBehaviour 
+public class UpdatedHealthScript : MonoBehaviour 
 {
-
-    public float health = 100;
-
+	
+	public float health = 100;
+	
 	// Use this for initialization
 	void Start () 
-    {
-	
+	{
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
-    {
-        if (this.GetComponent<VirusScript>().Blueberry == true && health > 0)
-        {
-            health -= 0.1f;
-        }
-
-        if (health <=  0)
-        {
-            Debug.Log(this.name + " Died");
-        }
-
-
-
-
+	{
+		if (this.GetComponent<VirusScript>().Blueberry == true && health > 0)
+		{
+			health -= 1.1f;
+		}
+		
+		if (health <=  0)
+		{
+			Debug.Log(this.name + " Died");
+			Destroy (gameObject);
+			health = 0;
+		}
+		
+		
+		
+		
 	}
-
+	
 	void OnGUI ()
 	{
 		//GUI.Label (new Rect (85, 100, 100, 30),"Player Health:"); 
 		GUI.Label (new Rect (180, 100, 300, 30),"Player Health:  " +health.ToString());    
-
+		
 	}
 }
