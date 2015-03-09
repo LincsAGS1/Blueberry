496b01ddec4be3b49700dc1c1ab373d808d4fb91
Connor Slater <slaterconnor@talktalk.net>
2015-03-09 19:27:46 +0000

    Added Alex's Stuff
    Didn't delete the menu stuff, and co-op is not yet implemented, but it
    is in the game.

--- Blueberry/Assets/Scripts/Player Scripts/PlayerController.cs
+++ Blueberry/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -5,11 +5,15 @@ using KinectForWheelchair;
 
 public class PlayerController : MonoBehaviour
 {
+
 	public InputController inputController;
 
     public bool kinectEnabled = true;
     public bool controllerEnabled = true;
-    public float turnMultiplier;
+	public bool controllerEnabled2 = true; 
+
+    public bool canMove = true;
+	public bool canMove2 = true;
 
 	// Use this for initialization
 	void Start ()
@@ -18,50 +22,95 @@ public class PlayerController : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update ()
-	{
-        if (controllerEnabled == true)
+    void Update()
+    {
+
+        if (canMove == true)
         {
-            if (Input.GetAxis("Joystick Triggers") > 0.05)
+
+            this.transform.Translate(Vector3.up * 0.03f);
+
+            if (controllerEnabled == true)
             {
-                this.transform.Rotate(Vector3.forward * (Input.GetAxis("Joystick Triggers") * turnMultiplier));
+                if (Input.GetAxis("Joystick Triggers") > 0.05)
+                {
+                    this.transform.Rotate(Vector3.forward * (Input.GetAxis("Joystick Triggers") * 5));
+                }
+                else if (Input.GetAxis("Joystick Triggers") < -0.05)
+                {
+                    this.transform.Rotate(Vector3.forward * (Input.GetAxis("Joystick Triggers") * 5));
+                }
+                //Debug.Log(Input.GetAxis("Joystick Triggers"));
             }
-            else if (Input.GetAxis("Joystick Triggers") < -0.05)
+
+            // Get the input info
+            SeatedInfo inputInfo = this.inputController.InputInfo;
+            if (inputInfo == null)
+                return;
+
+            // Set the player position and direction
+            if (inputInfo.Features == null)
+                return;
+
+
+			if (canMove2 == true)
+			{
+				
+				this.transform.Translate(Vector3.up * 0.03f);
+				
+				if (controllerEnabled2 == true)
+				{
+					if (Input.GetAxis("Joystick Triggers") > 0.05)
+					{
+						this.transform.Rotate(Vector3.forward * (Input.GetAxis("Joystick Triggers") * 5));
+					}
+					else if (Input.GetAxis("Joystick Triggers") < -0.05)
+					{
+						this.transform.Rotate(Vector3.forward * (Input.GetAxis("Joystick Triggers") * 5));
+					}
+					//Debug.Log(Input.GetAxis("Joystick Triggers"));
+				}
+				
+				// Get the input info
+				SeatedInfo inputInfo2 = this.inputController.InputInfo;
+				if (inputInfo2 == null)
+					return;
+				
+				// Set the player position and direction
+				if (inputInfo2.Features == null)
+					return;
+			
+
+            //Debug.Log (inputInfo.Features.Position);
+            //this.transform.position = new Vector3(inputInfo.Features.Position.x, 0, inputInfo.Features.Position.y) * 5;
+            //this.transform.forward = new Vector3(inputInfo.Features.Direction.x, 0, inputInfo.Features.Direction.y) * 5;
+
+            if (kinectEnabled == true)
             {
-                this.transform.Rotate(Vector3.forward * (Input.GetAxis("Joystick Triggers") * turnMultiplier));
+
+                if (inputInfo.Features.Angle > 5)
+                {
+                    //this.transform.rotation = new Quaternion(0, 0, this.transform.rotation.z + inputInfo.Features.Angle, 0.1f);
+                    this.transform.Rotate(Vector3.back * (inputInfo.Features.Angle / 20));
+                }
+                else if (inputInfo.Features.Angle < -5)
+                {
+                    //this.transform.rotation = Quaternion(0, 0, this.transform.rotation.z + inputInfo.Features.Angle, 0.1f);
+                    this.transform.Rotate(Vector3.back * (inputInfo.Features.Angle / 20));
+                }
+                //this.transform.rotation = new Quaternion(0, 0, inputInfo.Features.Angle,0);
+                Debug.Log(inputInfo.Features.Angle);
             }
-            //Debug.Log(Input.GetAxis("Joystick Triggers"));
+
+
+
+            return;
         }
-
-		// Get the input info
-		SeatedInfo inputInfo = this.inputController.InputInfo;
-		if (inputInfo == null)
-			return;
-
-		// Set the player position and direction
-		if (inputInfo.Features == null)
-			return;
-
-		//Debug.Log (inputInfo.Features.Position);
-		//this.transform.position = new Vector3(inputInfo.Features.Position.x, 0, inputInfo.Features.Position.y) * 5;
-		//this.transform.forward = new Vector3(inputInfo.Features.Direction.x, 0, inputInfo.Features.Direction.y) * 5;
-
-        if (kinectEnabled == true)
+        else
         {
-            if (inputInfo.Features.Angle > 5)
-            {
-                //this.transform.rotation = new Quaternion(0, 0, this.transform.rotation.z + inputInfo.Features.Angle, 0.1f);
-                this.transform.Rotate(Vector3.back * (inputInfo.Features.Angle / 20));
-            }
-            else if (inputInfo.Features.Angle < -5)
-            {
-                //this.transform.rotation = Quaternion(0, 0, this.transform.rotation.z + inputInfo.Features.Angle, 0.1f);
-                this.transform.Rotate(Vector3.back * (inputInfo.Features.Angle / 20));
-            }
-            //this.transform.rotation = new Quaternion(0, 0, inputInfo.Features.Angle,0);
-            Debug.Log(inputInfo.Features.Angle);
-        }     
-
-		return;
-	}
+            this.transform.Translate(0,0,0);
+        }
+    }
 }
+}
+
