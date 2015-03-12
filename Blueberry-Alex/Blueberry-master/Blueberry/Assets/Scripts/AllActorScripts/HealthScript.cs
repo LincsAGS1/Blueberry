b07f61b249ce5aa7b2703aa42012397b4c1ea8ca
Alex Rimes <alexrimes123@hotmail.com>
2015-03-12 09:50:07 +0000

    Added stuff
    Co-op (Needs testing) , Delay (Unfinished) , Highscore (Should work)

---
+++ Blueberry-Alex/Blueberry-master/Blueberry/Assets/Scripts/AllActorScripts/HealthScript.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour 
+{
+	public float health = 100;
+	
+	public AudioClip dontFeelGood;
+	public AudioClip feelingBlue;
+	public AudioClip helpMe;
+	public AudioClip deathSound;
+	
+	
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Lose health if infected
+		if (this.GetComponent<CollisionManager>().infected == true && health > 0)
+		{
+			health -= 0.1f;		
+		}
+		
+		
+		if (this.GetComponent<CollisionManager>().infected == true && health > 75 && health < 80)
+		{
+			AudioSource.PlayClipAtPoint(dontFeelGood,transform.position);
+		}
+		
+		if (this.GetComponent<CollisionManager>().infected == true && health > 45 && health <50)
+		{
+			AudioSource.PlayClipAtPoint(feelingBlue,transform.position);
+		}
+		
+		
+		if (this.GetComponent<CollisionManager>().infected == true && health > 25 && health <30)
+		{
+			AudioSource.PlayClipAtPoint(helpMe,transform.position);
+		}
+		
+		if (this.GetComponent<CollisionManager>().infected == true && health > 0 && health <5)
+		{
+			AudioSource.PlayClipAtPoint(deathSound,transform.position);
+		}
+		
+		if (this.GetComponent<CollisionManager>().infected == true && health > 0)
+			
+			
+			
+			//If dead, become blueberry (can't lose virus)
+			if (health <= 0)
+		{
+			this.GetComponent<CollisionManager>().blueberry = true;
+			//Debug.Log(this.name + " Dead");
+			//Destroy(gameObject);
+			health = 0;
+		}
+	}
+	
+	void OnGUI ()
+	{
+		//If this is a player, display player health
+		if (this.tag.Equals ("Player")) 
+		{
+		    //GUI.Label (new Rect (85, 100, 100, 30),"Player Health:");  
+			GUI.Label (new Rect (200, 30, 200, 30), "Player 1 Health:  " + health.ToString ());
+		} 
+		else if (this.tag.Equals ("Player 2")) 
+		{ 
+		    GUI.Label (new Rect (480, 30, 200, 30), "Player 2 Health:  " + health.ToString ());
+
+			}
+		}
+	}
+
+
---
+++ Blueberry-Alex/Blueberry-master/Blueberry/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using KinectForWheelchair;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public InputController inputController;
+
+    public bool kinectEnabled = true;
+	public bool controllerEnabled = true;
+
+    public bool canMove = true;
+	public bool canMove2 = true;
+
+	// Use this for initialization
+	void Start ()
+	{
+		return;
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+
+        if (canMove == true)
+        {
+
+            this.transform.Translate(Vector3.up * 0.03f);
+
+            if (controllerEnabled == true)
+            {
+                if (Input.GetAxis("Joystick Triggers") > 0.05)
+                {
+                    this.transform.Rotate(Vector3.forward * (Input.GetAxis("Joystick Triggers") * 5));
+                }
+                else if (Input.GetAxis("Joystick Triggers") < -0.05)
+                {
+                    this.transform.Rotate(Vector3.forward * (Input.GetAxis("Joystick Triggers") * 5));
+                }
+                //Debug.Log(Input.GetAxis("Joystick Triggers"));
+            }
+
+            // Get the input info
+            SeatedInfo inputInfo = this.inputController.InputInfo;
+            if (inputInfo == null)
+                return;
+
+            // Set the player position and direction
+            if (inputInfo.Features == null)
+                return;
+
+
+
+			else if (canMove2 == true)
+			{
+				
+				this.transform.Translate(Vector3.up * 0.05f);
+				
+				if (controllerEnabled == true)
+				{
+					if (Input.GetAxis("Joystick Triggers") > 0.05)
+					{
+						this.transform.Rotate(Vector3.forward * (Input.GetAxis("Joystick Triggers") * 5));
+					}
+					else if (Input.GetAxis("Joystick Triggers") < -0.05)
+					{
+						this.transform.Rotate(Vector3.forward * (Input.GetAxis("Joystick Triggers") * 5));
+					}
+					//Debug.Log(Input.GetAxis("Joystick Triggers"));
+				}
+			}
+				
+				// Get the input info
+				SeatedInfo inputInfo2 = this.inputController.InputInfo;
+				if (inputInfo == null)
+					return;
+				
+				// Set the player position and direction
+				if (inputInfo2.Features == null)
+					return;
+
+            //Debug.Log (inputInfo.Features.Position);
+            //this.transform.position = new Vector3(inputInfo.Features.Position.x, 0, inputInfo.Features.Position.y) * 5;
+            //this.transform.forward = new Vector3(inputInfo.Features.Direction.x, 0, inputInfo.Features.Direction.y) * 5;
+
+            if (kinectEnabled == true)
+            {
+
+                if (inputInfo.Features.Angle > 5)
+                {
+                    //this.transform.rotation = new Quaternion(0, 0, this.transform.rotation.z + inputInfo.Features.Angle, 0.1f);
+                    this.transform.Rotate(Vector3.back * (inputInfo.Features.Angle / 20));
+                }
+                else if (inputInfo.Features.Angle < -5)
+                {
+                    //this.transform.rotation = Quaternion(0, 0, this.transform.rotation.z + inputInfo.Features.Angle, 0.1f);
+                    this.transform.Rotate(Vector3.back * (inputInfo.Features.Angle / 20));
+                }
+                //this.transform.rotation = new Quaternion(0, 0, inputInfo.Features.Angle,0);
+                Debug.Log(inputInfo.Features.Angle);
+            }
+
+
+            return;
+        }
+        else
+        {
+            this.transform.Translate(0,0,0);
+        }
+    }
+}
+
+
