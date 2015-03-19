73b716290922ca3d894d55e5c739778ac51c6196
FlamingGear <flaminggear@hotmail.co.uk>
2015-03-19 00:07:15 +0000

    Added Room Random Generation
    Created a random Generation System that works with all rooms.
    The exception to this is the Kitchen Scene which is currently missing?
    Shall fix ASAP.
    
    Also restructured the folders massively to be more useful. PLEASE take
    note of this guys, as we don't need folders for all scripts, unity has a
    function which will show you them. Things are now ordered by what
    objects they relate to; all enemy images & code is now in the Enemies
    folder, all Powerup images & code is now in the Powerups folder etc.

---
+++ Blueberry/Assets/AllActors/HealthScript.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour 
+{
+	public float health = 100;
+	
+	public AudioClip dontFeelGood;
+	public AudioClip feelingBlue;
+	public AudioClip helpMe;
+	public AudioClip deathSound;
+	public Vector3 pos;
+	public Texture healthbar;
+	
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (this.gameObject.tag == "AI") {
+
+
+			
+			pos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x-1, transform.position.y + 1, transform.position.z));
+			                                     pos.y = Screen.height - pos.y; 
+
+				}
+
+		//Lose health if infected
+		if (this.GetComponent<CollisionManager>().infected == true && health > 0)
+		{
+			health -= 0.01f;		
+		}
+		
+		
+		if (this.GetComponent<CollisionManager>().infected == true && health > 75 && health < 80)
+		{
+			AudioSource.PlayClipAtPoint(dontFeelGood,transform.position);
+		}
+		
+		if (this.GetComponent<CollisionManager>().infected == true && health > 45 && health <50)
+		{
+			AudioSource.PlayClipAtPoint(feelingBlue,transform.position);
+		}
+		
+		
+		if (this.GetComponent<CollisionManager>().infected == true && health > 25 && health <30)
+		{
+			AudioSource.PlayClipAtPoint(helpMe,transform.position);
+		}
+		
+		if (this.GetComponent<CollisionManager>().infected == true && health > 0 && health <5)
+		{
+			AudioSource.PlayClipAtPoint(deathSound,transform.position);
+		}
+		
+		if (this.GetComponent<CollisionManager>().infected == true && health > 0)
+			
+			
+			
+			//If dead, become blueberry (can't lose virus)
+			if (health <= 0)
+		{
+			this.GetComponent<CollisionManager>().blueberry = true;
+			//Debug.Log(this.name + " Dead");
+			//Destroy(gameObject);
+			health = 0;
+		}
+	}
+	
+	void OnGUI ()
+	{
+		//If this is a player, display player health
+		if (this.tag.Equals("Player"))
+		{
+			//GUI.Label (new Rect (85, 100, 100, 30),"Player Health:"); 
+			GUI.Label (new Rect (480, 30, 200, 30),"Player Health:  " +health.ToString());
+		}
+
+			if (this.tag == "AI")
+			GUI.DrawTexture(new Rect(pos.x,pos.y,1*this.health/2,40),healthbar, ScaleMode.ScaleToFit, true, 10.0F);
+
+	}
+}
---
+++ Blueberry/Assets/Powerups/Powerup.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+	public float timer = 5;
+	public GameObject GameManager;
+	public AudioClip sound;
+
+	// Use this for initialization
+	void Start () {
+		GameManager  = GameObject.FindWithTag("GameController");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer <= 0f)
+			Destroy (gameObject);
+	}
+
+	void OnCollisionEnter (Collision other)
+	{
+
+		AudioSource.PlayClipAtPoint(sound,transform.position);
+		AudioSource.PlayClipAtPoint(sound,transform.position);
+		if (other.gameObject.tag == "Player")
+		{}
+		//	GameManager.GetComponent<RandomVirus>().points += 15;
+	}
+}
