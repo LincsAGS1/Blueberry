ca78391af5181585a85a7e2458eef2189f67646c
Adam Bowes <skylinegames@hotmail.co.uk>
2015-04-22 15:06:13 +0100

    changes

--- Blueberry/Assets/AI/EnemyAI.cs
+++ Blueberry/Assets/AI/EnemyAI.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class EnemyAI : MonoBehaviour 
 {
 	public float rotationLerpScale;
+	private float randomTimer = 0.0f;
+	private Vector2 randomVec = Random.insideUnitCircle;
         
 	void Awake() 
     {
@@ -27,47 +29,91 @@ public class EnemyAI : MonoBehaviour
             if (this.GetComponent<AgentManager>().infected == false)
             {
                 //If NOT a carrier
-                ArrayList infected = new ArrayList();
+                ArrayList agents = new ArrayList();
                 ArrayList obstacles = new ArrayList();
                 ArrayList avoidVectors = new ArrayList();
 
+
                 //Avoid things that ARE blueberries with highest priority
                     //Get all enemies & Add each infected to a list of infected
-                    infected.AddRange(GameObject.FindGameObjectsWithTag("AI"));
+					agents.AddRange(GameObject.FindGameObjectsWithTag("AI"));
                         
                     //Get all players & Add each infected to a list of infected
-                    infected.AddRange(GameObject.FindGameObjectsWithTag("Player"));
+                    agents.AddRange(GameObject.FindGameObjectsWithTag("Player"));
                     
                     //Get vectors FROM infected TO agent & add to list of vectors
                     //Vector = Destination - Origin
-                    foreach(GameObject obj in infected)
+                    foreach(GameObject obj in agents)
                     {
-                        avoidVectors.Add(new Vector2(this.transform.position.x - obj.transform.position.x,
-                                                     this.transform.position.y - obj.transform.position.y));
-                    }
+						Vector2 newVector = new Vector2(this.transform.position.x - obj.transform.position.x, this.transform.position.y - obj.transform.position.y);	
+
+						if (obj.GetComponent<AgentManager>().infected && newVector.magnitude < 4)
+						{
+							Debug.Log ("infected");
+							
+							
+							//Invert magnitudes of obstacles (further distance means lower priority avoidance)
+							//23 is maximum distance within game world (from one corner to diagonal corner)
+							//float magnitude = newVector.magnitude;
+							newVector.Normalize();
+							//newVector *= (23 - magnitude);
+							
+							//Scale magnuitudes to be 1/5 of those of the infected 
+							//(avoiding obstacles is lower priority than avoiding the infected)
+							newVector *= 2;
+							
+							avoidVectors.Add(newVector);
+						}       
+					}
+
+					randomTimer -= Time.deltaTime;
+
+					if (avoidVectors.Count == 0)
+					{
+						if ( randomTimer < 0.0f )
+						{
+							randomVec = Random.insideUnitCircle;
+							randomTimer = Random.Range (0.5f, 2.0f);
+							avoidVectors.Add (randomVec * 4);
+						}
+						else 
+						{
+							avoidVectors.Add (randomVec * 4);
+						}
+					}
+						
+
                                         
                 //Also avoid obstacles, proportional to how close they are. 
                 //(Inversely proportional to the magnitude of the vector between them)
 
                 //Get all obstacles
                 obstacles.AddRange(GameObject.FindGameObjectsWithTag("Obstacle"));
+				obstacles.AddRange(GameObject.FindGameObjectsWithTag("corner"));
 
                 //Get vectors FROM obstacles TO agent  
-                foreach (GameObject obj in obstacles)
+                /*foreach (GameObject obj in obstacles)
                 {
-                    Vector2 newVector = new Vector2(obj.transform.position.x, obj.transform.position.y);
+					Vector2 newVector = new Vector2(this.transform.position.x - obj.transform.position.x, this.transform.position.y - obj.transform.position.y);
 
                     //Invert magnitudes of obstacles (further distance means lower priority avoidance)
                     //23 is maximum distance within game world (from one corner to diagonal corner)
                     float magnitude = newVector.magnitude;
-                    newVector.Normalize();
-                    newVector *= (23 - magnitude);
+                    
+                    magnitude = 23-magnitude;
                     //Scale magnuitudes to be 1/5 of those of the infected 
                     //(avoiding obstacles is lower priority than avoiding the infected)
-                    newVector *= (1/5);
 
-                    avoidVectors.Add(newVector);
-                }
+
+						newVector.Normalize();
+						newVector *= 1f*magnitude;
+
+
+
+	                avoidVectors.Add(newVector);
+
+
+                }*/
 
                 //Sum vectors to get an overall movement vector
                 Vector2 resultantVector = new Vector2();
